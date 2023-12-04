82dcddd374ee0556243ec572ed18d8dab64a4fd0
karastoyanov on macbookPRO <karastoqnov.alexandar@gmail.com>
2023-12-05 01:23:57 +0200

    dir restructure

--- Terraform/web-servers.tf
+++ Terraform/web-servers.tf
@@ -7,19 +7,19 @@ resource "digitalocean_droplet" "apache-server" {
   size     = "s-1vcpu-1gb"
   ssh_keys = [digitalocean_ssh_key.terraform.fingerprint]
 
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /web-setup/web_bootstrap.sh",
-      "./web-setup/web_bootstrap.sh",
-    ]
-  }
-
   connection {
     type        = "ssh"
     user        = "root"
-    private_key = file(var.pvt_key) # Update with the path to your private key
-    host        = digitalocean_droplet.apache-server[count.index].ipv4_address
+    private_key = file(var.pvt_key)
+    host        = element(digitalocean_droplet.apache-server.*.ipv4_address, count.index)
   }
+
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "chmod +x /web-setup/web_bootstrap.sh",
+  #     "./web-setup/web_bootstrap.sh",
+  #   ]
+  # }
 }
 
 
@@ -28,27 +28,17 @@ resource "null_resource" "copy_files_wb_boostrap" {
   count = length(digitalocean_droplet.apache-server)
 
   provisioner "file" {
-    source      = "Draft/CompanyWebSite/WebSiteProject/wb_bootstrap.sh" # Update with the path to your local file
-    destination = "/web-setup/"                                         # Update with the desired path on the remote Droplet
+    source      = "Draft/CompanyWebSite/WebSiteProject/wb_bootstrap.sh"
+    destination = "/web-setup/"
   }
-}
-
-# Copy website file to the /var/www directory
-resource "null_resource" "copy_files_website_dir" {
-  count = length(digitalocean_droplet.apache-server)
 
   provisioner "file" {
-    source      = "Services/CompanyWebSite/WebSiteProject/company-website" # Update with the path to your local file
-    destination = "/var/www/company-site/"                                 # Update with the desired path on the remote Droplet
+    source      = "Services/CompanyWebSite/WebSiteProject/company-website" 
+    destination = "/var/www/company-site/"
   }
-}
-
-# Copy the Apache conf 
-resource "null_resource" "copy_files_apache_conf_file" {
-  count = length(digitalocean_droplet.apache-server)
 
   provisioner "file" {
-    source      = "Services/CompanyWebSite/WebSiteProject/apache2.conf" # Update with the path to your local file
-    destination = "/etc/apache2/sites-available/trollparts.conf"        # Update with the desired path on the remote Droplet
+    source      = "Services/CompanyWebSite/WebSiteProject/apache2.conf"
+    destination = "/etc/apache2/sites-available/trollparts.conf"
   }
 }
