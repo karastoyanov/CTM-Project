88b6441bd5f0cbf5af338d5fa8beac7b59849cad
karastoyanov on macbookPRO <karastoqnov.alexandar@gmail.com>
2024-01-03 15:42:04 +0200

    Vagrant Docker&&Jenkins update

---
+++ Vagrant/Jenkins-Server/jenkins_bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt update && apt install wget -y
+
+wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+apt update && apt install jenkins fontconfig openjdk-17-jre -y
